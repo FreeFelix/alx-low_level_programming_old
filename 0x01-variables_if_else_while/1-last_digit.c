7a74163383eadab7e6f3957af6b1998c4da7eb28
FreeFelix <nijeanfelix@gmail.com>
2023-06-17 15:56:47 -0700

    Adding commited code from debugging

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 	}
 	else if ((n % 10) < 6 && (n % 6) != 0)
 	{
-	printf("Last digit of %d is %d and is less tahn 6 and not 0\n", n, n % 10);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
 	else
 	{
