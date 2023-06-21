624187ddb427325016ba7ae8f0ab0a352b67f50c
FreeFelix <nijeanfelix@gmail.com>
2023-06-20 20:08:00 -0700

    New commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - print_alphabet function That is going to lowercase Letter.
+ */
+void print_alphabet(void)
+{
+char Lett;
+for (Lett = 'a'; Letter <= 'z'; Lett++)
+{
+	_putchar(Lett);
+}
+_putchar('\n');
+}
