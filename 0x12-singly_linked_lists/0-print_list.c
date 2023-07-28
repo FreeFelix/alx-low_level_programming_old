bdf4ebef0192000bcb07f0a3f2956afec322e9eb
FreeFelix <nijeanfelix@gmail.com>
2023-07-28 05:57:44 -0700

    Another commit for deburg

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,12 +12,12 @@ size_t print_list(const list_t *h)
 size_t c = 0;
 while (h)
 {
-	if (!h->str)
-	printf("[0] (nil)\n");
-	else
-	printf("[%d] %s\n", h->len, h->str);
-	h = h->next;
-	c++;
+if (!h->str)
+printf("[0] (nil)\n");
+else
+printf("[%d] %s\n", h->len, h->str);
+h = h->next;
+c++;
 }
 return (c);
 }
