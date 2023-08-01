78be8ed00f0ddd030e7a9cfc3238c256549df354
FreeFelix <nijeanfelix@gmail.com>
2023-08-01 14:32:33 -0700

    My first commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,15 +7,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t data = 0;
+size_t data = 0;
 
-	while (h)
-	{
-		printf(" %d\n", h->n);
-		data++;
-		h = h->next;
-	}
-
-	return (data);
+while (h)
+{
+printf(" %d\n", h->n);
+data++;
+h = h->next;
+}
+return (data);
 }
 
